8d307115450440791e1c4618cc0e9152238163fb
StanlsSlav <65411828+StanlsSlav@users.noreply.github.com>
2023-11-25 19:24:16 +0100

    feat: add typing to parsing

--- src/models/Ticket.js
+++ src/models/Ticket.js
@@ -1,4 +1,8 @@
 export default class Ticket {
+  /**
+   *
+   * @param {{enter_date_time: number, customer_id: number}} params
+   */
   constructor({ enter_date_time, customer_id, article_id, quantity, id }) {
     this.enter_date_time = enter_date_time;
     this.customer_id = customer_id;
